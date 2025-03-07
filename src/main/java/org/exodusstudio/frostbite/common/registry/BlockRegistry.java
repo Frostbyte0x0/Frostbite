8a02034f796a9e88ec411fb2a35ce33c5473af4d
Berserk007 <la.tribu.des.berserk@gmail.com>
2025-03-06 20:23:48 -0500

    Found how to modify biome generation

--- src/main/java/org/exodusstudio/frostbite/common/registry/BlockRegistry.java
+++ src/main/java/org/exodusstudio/frostbite/common/registry/BlockRegistry.java
@@ -1,5 +1,7 @@
 package org.exodusstudio.frostbite.common.registry;
 
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.RotatedPillarBlock;
 import org.exodusstudio.frostbite.Frostbite;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
@@ -20,6 +22,19 @@ public class BlockRegistry {
     public static final DeferredBlock<Block> BLACK_BLOCK = registerBLock("black_block",
             () -> new Block(BlockBehaviour.Properties.of().strength(10f)
                     .setId(ResourceKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(Frostbite.MOD_ID, "black_block")))), "black_block");
+
+    public static final DeferredBlock<Block> HOARFROST_LOG = registerBLock("hoarfrost_log",
+            () -> new RotatedPillarBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_LOG)
+                    .setId(ResourceKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(Frostbite.MOD_ID, "hoarfrost_log")))), "hoarfrost_log");
+    public static final DeferredBlock<Block> HOARFROST_WOOD = registerBLock("hoarfrost_wood",
+            () -> new RotatedPillarBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_WOOD)
+                    .setId(ResourceKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(Frostbite.MOD_ID, "hoarfrost_wood")))), "hoarfrost_wood");
+    public static final DeferredBlock<Block> STRIPPED_HOARFROST_LOG = registerBLock("stripped_hoarfrost_log",
+            () -> new RotatedPillarBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.STRIPPED_OAK_LOG)
+                    .setId(ResourceKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(Frostbite.MOD_ID, "stripped_hoarfrost_log")))), "stripped_hoarfrost_log");
+    public static final DeferredBlock<Block> STRIPPED_HOARFROST_WOOD = registerBLock("stripped_hoarfrost_wood",
+            () -> new RotatedPillarBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.STRIPPED_OAK_WOOD)
+                    .setId(ResourceKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(Frostbite.MOD_ID, "stripped_hoarfrost_wood")))), "stripped_hoarfrost_wood");
     
     public static <T extends Block> DeferredBlock<T> registerBLock(String name, Supplier<T> block, String path) {
         DeferredBlock<T> toReturn = BLOCKS.register(name, block);
